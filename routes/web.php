0fbc6ba91bf98dac59833ae91fcfad7df82ae26a
ElmurodSuyunov01 <elmurodsuyunov02@gmail.com>
2022-04-17 23:55:48 -0700

    welcome

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('');
 });
 Route::get('/color', function () {
     return view('welcome');
